fcaee9ee8ae0e9ede5bb8ed1a7066fbc45e782d2
NoscanX <j.michalski7@gmail.com>
2022-03-12 17:58:07 +0100

    Navigation to pages added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,68 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_fillDataButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_moveToAlgotythmsButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(4);
+}
+
+
+void MainWindow::on_exitButton_clicked()
+{
+    this->close();
+}
+
+
+void MainWindow::on_autoFillDataButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_manualFillDataButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(5);
+}
+
+
+void MainWindow::on_backButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_backButton_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_backButton_3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_backButton_4_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_backButton_5_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(4);
+}
+
